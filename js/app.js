a709d867642c75439f17e6aef0b6d4239ae6e80c
michele <micheleepedrotti@gmail.com>
2021-04-19 12:16:22 +0200

    milestone 1

--- js/app.js
+++ js/app.js
@@ -3,6 +3,35 @@ function initVue() {
     new Vue({
 
         el: '#app',
+        data: {
+
+            apiKey: 'f64a77b6bcffaceefba175225998a0d4',
+            searchBarTxt: '',
+            filmsToShow: []
+        },
+        methods: {
+
+            searchTxt: function() {
+
+                axios.get('https://api.themoviedb.org/3/search/movie', {
+
+                    params: {
+                        'api_key': this.apiKey,
+                        'query': this.searchBarTxt
+                    }
+                })
+                .then((data) => {
+
+                    const matchingFilms = data.data.results;
+                    this.filmsToShow = matchingFilms
+                    console.log(this.filmsToShow);
+                })
+                .catch(() => {
+
+                    console.log('error');
+                });
+            },
+        },
     });
 };
 
